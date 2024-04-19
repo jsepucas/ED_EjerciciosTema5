ed34185d14469037bab0ebb3683a63272baef3b9
jsepucas <jsepucas@myuax.com>
2024-04-19 12:41:18 +0200

    Carpetas ejsss

--- src/main/java/A_SumaNumNaturales/SumaRecursivaNaturales.java
+++ src/main/java/A_SumaNumNaturales/SumaRecursivaNaturales.java
@@ -2,5 +2,13 @@ package A_SumaNumNaturales;
 
 public class SumaRecursivaNaturales {
 
+    public static int sumaRecursiva(int n) {
+        if (n <= 0) {
+            return 0; // Caso base: si n es 0 o negativo, la suma es 0
+        } else {
+            return n + sumaRecursiva(n - 1); // Paso recursivo
+        }
+    }
+
 
 }
