0ed0530d967c4b6c7c13ab3504e240fbb7493bd7
jsepucas <jsepucas@myuax.com>
2024-04-19 13:08:49 +0200

    Commits ejs

---
+++ src/main/java/B_ListaNumNaturales/ListaNumNaturalesGUI.java
@@ -0,0 +1,12 @@
+package B_ListaNumNaturales;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListaNumNaturalesGUI {
+
+    public static void main(String[] args) {
+
+
+    }
