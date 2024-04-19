8a3f2d14eb9709bb523ab16ac1571716bc211081
jsepucas <jsepucas@myuax.com>
2024-04-19 12:42:37 +0200

    Commits ejs

--- src/main/java/A_SumaNumNaturales/SumaRecursivaNaturalesGUI.java
+++ src/main/java/A_SumaNumNaturales/SumaRecursivaNaturalesGUI.java
@@ -7,4 +7,14 @@ import java.awt.event.ActionListener;
 
 
 public class SumaRecursivaNaturalesGUI {
-}
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Suma Recursiva de Números Naturales");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+
+
+
+
+    }
