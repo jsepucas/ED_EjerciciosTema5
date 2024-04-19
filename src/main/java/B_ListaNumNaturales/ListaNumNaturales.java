d2e6ef896c5d39c6bdc5a004cb5003d99a5ee65e
jsepucas <jsepucas@myuax.com>
2024-04-19 13:06:47 +0200

    Commits ejs

--- src/main/java/B_ListaNumNaturales/ListaNumNaturales.java
+++ src/main/java/B_ListaNumNaturales/ListaNumNaturales.java
@@ -2,13 +2,20 @@ package B_ListaNumNaturales;
 
 public class ListaNumNaturales {
 
-
     public static void imprimirNumeros(int a, int d) {
         if (a <= d) {
             System.out.print(a + " ");
-            imprimirNumeros(a + 1, d); // Llamada recursiva con incremento de 'a'
+            imprimirNumeros(a + 1, d);
         }
     }
 
-
+    public static void main(String[] args) {
+        int a = 5;
+        int d = 15;
+        System.out.println("Números naturales entre " + a + " y " + d + ":");
+        imprimirNumeros(a, d);
+    }
 }
+
+
+
