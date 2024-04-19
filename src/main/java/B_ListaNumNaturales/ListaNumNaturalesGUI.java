887c00a9928a71620a876e0373b94daa460b38d2
jsepucas <jsepucas@myuax.com>
2024-04-19 13:09:03 +0200

    Commits ejs

--- src/main/java/B_ListaNumNaturales/ListaNumNaturalesGUI.java
+++ src/main/java/B_ListaNumNaturales/ListaNumNaturalesGUI.java
@@ -8,5 +8,16 @@ public class ListaNumNaturalesGUI {
 
     public static void main(String[] args) {
 
+        JFrame frame = new JFrame("Lista de Números Naturales entre dos Valores");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+
+        JTextField inputFieldA = new JTextField(5);
+        JTextField inputFieldD = new JTextField(5);
+        JButton showListButton = new JButton("Mostrar Lista");
+        JTextArea resultArea = new JTextArea(5, 20);
+        resultArea.setEditable(false);
+    }
+
 
     }
