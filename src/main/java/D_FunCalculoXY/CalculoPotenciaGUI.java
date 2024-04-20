8e9dc58eccb68b63fd5cc7edc6bba48f32d838ca
jsepucas <jsepucas@myuax.com>
2024-04-20 13:46:01 +0200

    Commitss

--- src/main/java/D_FunCalculoXY/CalculoPotenciaGUI.java
+++ src/main/java/D_FunCalculoXY/CalculoPotenciaGUI.java
@@ -2,5 +2,15 @@ package D_FunCalculoXY;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+
 public class CalculoPotenciaGUI {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Cálculo de Potencia");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+
+
 }
