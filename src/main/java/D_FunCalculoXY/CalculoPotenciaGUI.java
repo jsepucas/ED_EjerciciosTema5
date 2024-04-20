fb5c9aee4617da8cfee7a11340a82d88e6dbc767
jsepucas <jsepucas@myuax.com>
2024-04-20 13:46:07 +0200

    Commitss

--- src/main/java/D_FunCalculoXY/CalculoPotenciaGUI.java
+++ src/main/java/D_FunCalculoXY/CalculoPotenciaGUI.java
@@ -12,5 +12,10 @@ public class CalculoPotenciaGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 200);
 
+        JTextField inputFieldX = new JTextField(5);
+        JTextField inputFieldY = new JTextField(5);
+        JButton calculateButton = new JButton("Calcular Potencia");
+        JLabel resultLabel = new JLabel("El resultado aparecerá aquí");
+
 
 }
