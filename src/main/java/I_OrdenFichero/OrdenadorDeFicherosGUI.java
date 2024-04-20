03d7171b0e68a29e3fa73ec765b0fbc6722d57b1
jsepucas <jsepucas@myuax.com>
2024-04-20 14:52:26 +0200

    Commitss

--- src/main/java/I_OrdenFichero/OrdenadorDeFicherosGUI.java
+++ src/main/java/I_OrdenFichero/OrdenadorDeFicherosGUI.java
@@ -1,4 +1,18 @@
 package I_OrdenFichero;
 
+import javax.swing.*;
+
 public class OrdenadorDeFicherosGUI {
-}
+
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("Ordenador de Ficheros");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 150);
+
+        JButton selectButton = new JButton("Seleccionar Archivo");
+        JLabel fileLabel = new JLabel("No se ha seleccionado ningún archivo.");
+        JButton orderButton = new JButton("Ordenar y Guardar");
+        orderButton.setEnabled(false);
+    }
